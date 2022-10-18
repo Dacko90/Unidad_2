94e6a316c712d91a3c262ce8dbbc57c0b498b81f
Dacko90 <Dacko90@users.noreply.github.com>
2022-10-18 09:22:21 -0500

    Create 03_list_dicts.py
    python

---
+++ 03_list_dicts.py
@@ -0,0 +1,12 @@
+space = ""
+
+print("¿Como te llamas?:")
+name = input()
+print("¿Cual es tu apellido?:")
+lastn = input()
+print("¿De donde eres?:")
+location = input()
+print("¿Cual es tu edad?:")
+age = input()
+
+print ("Se llama " + name + " y su apellido es " + lastn + " es originario de " + location + " y tiene " + age + " años.")
