16cc92f439ca30f737bef331e34d4e0a1fb4a379
Dacko90 <Dacko90@users.noreply.github.com>
2022-10-18 09:02:11 -0500

    Update 02_list-dicts.py
    python

--- 02_list-dicts.py
+++ 02_list-dicts.py
@@ -17,7 +17,7 @@ Why did the list and dictionary contents not print?
 Fix the code and run the script again.
 """
 
-print( capitals ["South Africa"] )
+print( capitals ["South Africa"] [1] )
 """
 Why did you get an error for the 2nd capital of South Africa?
 Hint: Check the syntax for the index value.
