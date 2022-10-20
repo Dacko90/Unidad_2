7ad787946cf90fc0802973571650d6771cbe256c
Dacko90 <Dacko90@users.noreply.github.com>
2022-10-20 08:58:36 -0500

    Update 07_list_dicts.py

--- 07_list_dicts.py
+++ 07_list_dicts.py
@@ -6,15 +6,14 @@ Descripción: imprimir lineas de otro documento
 '''
 file = open("devices.txt", "r")
 dispositivo =  input("Dime el dispositivo a encontrar: ")
+isfound = False
 
-
-for line in file:
+for line in file :
     line = line.strip()
-    
-        
-if dispositivo in line:
-    print(line)
-else:
+    if dispositivo in line:
+        isfound = True
+        print(line)
+if isfound==False:
     print("Dispositivo no encontrado")
             
 
